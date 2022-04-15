b3f09827c63eee0e0e23779626887279cba1df11
Felipe Restrepo <manuel.restrepo@carvajal.com>
2022-04-14 22:38:01 -0500

    Draw spline points

--- flaskr/upload.py
+++ flaskr/upload.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 from os.path import join, dirname, realpath
 
@@ -10,15 +11,34 @@ from flask import request
 from flask import send_from_directory
 from flask import url_for
 
+from scipy import interpolate
 from werkzeug.utils import secure_filename
 
 bp = Blueprint("upload", __name__)
 
+t = [0,0,0,0,1,1,1,1]
+c = [[58.3863029989894,116.89745219181054],[105.31685340513181,38.956713078315055],[310.3370211379565,22.44508241175887],[369.9147409302334,111.62765304361334]]
+c_int = np.array(c)
+k = 3
+
 def draw_spline(file_path):
     img = cv2.imread(file_path, 1)
     height, width, channels = img.shape
+
+    x_range = np.arange(width)
+    x_range_new = np.arange(width)
+    y_range = interpolate.splev(x_range_new, (t, c, k), der=0)
+
+    print(len(y_range))
     red = (0, 0, 255)
-    cv2.line(img, (height, 0), (0, width), red, 3)
+
+    draw_points = (np.asarray([c_int[:,0], c_int[:,1]]).T).astype(np.int32)
+    cv2.polylines(img, [draw_points], False, (0,0,0))  # args: image, points, closed, color
+
+    for n in range(len(y_range)):
+        draw_points = (np.asarray([x_range, y_range[n]]).T).astype(np.int32)   # needs to be int32 and transposed
+        cv2.polylines(img, [draw_points], False, (0,0,0))  # args: image, points, closed, color
+
     cv2.imwrite(file_path, img)
 
 def allowed_file(filename):
@@ -51,6 +71,7 @@ def upload_file():
     <form method=post enctype=multipart/form-data>
       <input type=file name=file>
       <input type=submit value=Upload>
+      <input type=text value=Param>
     </form>
     '''
 
